1b1aa1f2135a950be8342149947bbf43687ad707
mdumar476 <mdumarf476@gmail.com>
2022-06-01 17:02:47 +0530

    This is my second commit

--- TestGitProject/src/com/demo/GitSample.java
+++ TestGitProject/src/com/demo/GitSample.java
@@ -2,7 +2,8 @@ package com.demo;
 
 public class GitSample {
 	public static void main(String[] args) {
-		System.out.println("This is my first git project");
+		System.out.println("Main begin");
+		System.out.println("Main end");
 	}
   
 }
